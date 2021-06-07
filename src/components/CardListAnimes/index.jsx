8248ba57548217a5290d16bbd77b9392d6564dd5
Matheus Dantas <correia.dantas@outlook.com>
2021-06-07 01:11:24 -0300

    feat: adicionando loading circular

--- src/components/CardListAnimes/index.jsx
+++ src/components/CardListAnimes/index.jsx
@@ -1,6 +1,7 @@
 import CardAnime from "../CardAnime";
 import { useAnime } from "../../providers/Animes";
 import { Container } from "./styles";
+import LoadingCircular from "../LoadingCircular";
 
 const CardListAnimes = () => {
   const { animes, load } = useAnime();
@@ -8,14 +9,17 @@ const CardListAnimes = () => {
   console.log(animes);
   return (
     <Container>
-      {load &&
+      {!load ? (
+        <LoadingCircular />
+      ) : (
         animes?.map((i) => (
           <CardAnime
             anime={i.anime}
             date={i.released_date}
             seasons={i.seasons}
           />
-        ))}
+        ))
+      )}
     </Container>
   );
 };
